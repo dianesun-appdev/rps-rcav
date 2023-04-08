8c1526b1c4548f29d1ac348c16a3b8ecf83578c2
sun-diane Sun <dianesun1@gmail.com>
2023-04-08 22:07:17 +0000

    Added RPS pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,51 @@ class ApplicationController < ActionController::Base
   # ================================
 
   def play_rock
+    @user_move = "rock"
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "tied"
+    elsif @computer_move == "paper"
+      @outcome = "lost"
+    else
+      @outcome = "won"
+    end
+
+    render({ :template => "game_templates/rps_game.html.erb" })
   end
 
   def play_scissors
+    @user_move = "scissors"
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "lost"
+    elsif @computer_move == "paper"
+      @outcome = "won"
+    else
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/rps_game.html.erb" })
   end
 
   def play_paper
+    @user_move = "paper"
+    @computer_move = ["rock", "paper", "scissors"].sample
+
+    if @computer_move == "rock"
+      @outcome = "won"
+    elsif @computer_move == "paper"
+      @outcome = "tied"
+    else
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/rps_game.html.erb" })
   end
 
   def homepage
+    render({ :template => "game_templates/homepage.html.erb" })
   end
 end
