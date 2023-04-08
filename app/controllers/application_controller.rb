fcb1349e6a67a186bb188debf3451c4d7f657876
sun-diane Sun <dianesun1@gmail.com>
2023-04-08 21:34:16 +0000

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,15 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def play_rock
+  end
+
+  def play_scissors
+  end
+
+  def play_paper
+  end
+
+  def homepage
+  end
 end
